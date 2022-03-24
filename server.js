471b2e2f06fb62a85fbecd018b25ff8a1bc79a4c
yeeterlol <81702700+yeeterlol@users.noreply.github.com>
2022-03-24 15:51:26 -0700

    fix

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const express = require("express")();
+const express = require('express')
 const app = express()
 const port = 3000
-// Remove .html and .html
+
 app.use(express.static("public", {
     extensions: ['html', 'htm'],
-    // Other options here
 }));
 
 app.get('/', (req, res) => {
