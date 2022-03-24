8796f832873399e07f79b1d774c92b9287d395c1
yeeterlol <81702700+yeeterlol@users.noreply.github.com>
2022-03-24 15:56:26 -0700

    No errors please

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express')();
-const app = express()
+const express = require('express');
+
+const app = require('express')();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public", {
